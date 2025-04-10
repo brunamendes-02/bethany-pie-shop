e71e00c3c50e3d2f8e3c91ad9b7093573752c8a8
brunamendes-02 <bruna.kcn@gmail.com>
2025-04-10 09:04:04 -0300

    working with lists

--- Program.cs
+++ Program.cs
@@ -1,25 +1,6 @@
 ﻿using BethanysPieShopHRM.HR;
 
-// int[] sampleArray1 = new int[5];
-// int[] sampleArray2 = new int[] { 1, 2, 3, 4, 5 };
-// Console.WriteLine("How many employees IDs you want to register?");
-
-// int length = int.Parse(Console.ReadLine());
-// int[] ids = new int[length];
-
-// for (int i = 0; i < length; i++)
-// {
-//     Console.WriteLine("Enter the employee id");
-//     int id = int.Parse(Console.ReadLine());
-//     ids[i] = id;
-
-// }
-
-
-// for (int i = 0; i < length; i++)
-// {
-//     Console.WriteLine($"Id: " + ids[i]);
-// }
+List<Employee> employees = new List<Employee>();
 
 Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.be", new DateTime(1979, 1, 16), 25, EmployeeType.Manager);
 Employee george = new Employee("George", "Jones", "george@snowball.be", new DateTime(1984, 3, 28), 30, EmployeeType.Research);
@@ -29,17 +10,14 @@ Employee kevin = new Employee("Kevin", "Marks", "kevin@snowball.be", new DateTim
 Employee kate = new Employee("Kate", "Greggs", "kate@snowball.be", new DateTime(1993, 8, 8), 10, EmployeeType.StoreManager);
 Employee kim = new Employee("Kim", "Jacobs", "kim@snowball.be", new DateTime(1975, 5, 14), 22, EmployeeType.StoreManager);
 
-Employee[] employees = new Employee[7];
-employees[0] = bethany;
-employees[1] = george;
-employees[2] = mary;
-employees[3] = bobJunior;
-employees[4] = kevin;
-employees[5] = kate;
-employees[6] = kim;
+employees.Add(bethany);
+employees.Add(george);
+employees.Add(mary);
+employees.Add(bobJunior);
+employees.Add(kevin);
+employees.Add(kate);
+employees.Add(kim);
 
-foreach(Employee e in employees) {
-    e.DisplayEmployeeDetails();
-    var numberOfHoursWorked = new Random().Next(25);
-    e.PerformWork(numberOfHoursWorked);
+foreach(Employee employee in employees) {
+    employee.DisplayEmployeeDetails();
 }
